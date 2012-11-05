5358dd2dc3f04b1ead38e2d255237b816253bde7
Richard Sim <rsim@Richard-Sims-MacBook-Pro.local>
2012-11-04 22:29:01 -0800

    Initial Commit

---
+++ TankAI/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  TankAI
+//
+//  Created by Richard Sim on 12-11-04.
+//  Copyright (c) 2012 Mesophase. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
